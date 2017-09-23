4737fe920a128a6ee91cef8bd1202b0fa10e4309
Anthony <anthony.vallario@hotmail.com>
2017-09-22 23:50:57 -0400

    Added materials and updated scene

--- raymarcher.js
+++ raymarcher.js
@@ -55,6 +55,42 @@ class ray {
 	}
 }
 
+class material {
+	constructor(ambient, diffuse, specular, specpow) {
+		this.ambient = ambient;
+		this.diffuse = diffuse;
+		this.specular = specular;
+		this.specpow = specpow;
+	}
+}
+
+class sphere {
+	constructor(origin, radius, material) {
+		this.origin = origin;
+		this.init_origin = new vec3(origin.x, origin.y, origin.z);
+		this.radius = radius;
+		this.material = material;
+	}
+
+	sdf(point) {
+		return vec3.sub(this.origin, point).length - this.radius;
+	}
+}
+
+class sdf_record {
+	constructor(distance, material) {
+		this.distance = distance;
+		this.material = material;
+	}
+}
+
+class intersection_record {
+	constructor(intersection_point, material) {
+		this.intersection_point = intersection_point;
+		this.material = material;
+	}
+}
+
 let c = document.getElementById("rmCanvas");
 let ctx = c.getContext("2d");
 const WINDOW_WIDTH = c.width;
@@ -69,24 +105,22 @@ const EPSILON = 0.000001;
 const GAMMA = 2.2
 const GAMMA_CORRECT = false;
 
-//Test data
-let sphere_center_start = new vec3(0, 0, 25);
-let sphere_center = new vec3(sphere_center_start.x, sphere_center_start.y, sphere_center_start.z);
-let sphere_radius = 0.9;
+let camera_center = new vec3(0, 0, -5);
 
-let sphere2_center_start = new vec3(0, 0, 25);
-let sphere2_center = new vec3(sphere2_center_start.x, sphere2_center_start.y, sphere2_center_start.z);
-let sphere2_radius = 0.9;
-
-let sphere_ambient = [0.025, 0.04, 0.075];
-let sphere_diffuse = [0.1, 0.3, 0.5];
-let sphere_specular = [0.3, 0.3, 0.3];
-let sphere_specpow = 16;
-
+//Scene data
 let light_center = new vec3(-3, -3, 17)
 let light_color = [1, 1, 1]
 
-let camera_center = new vec3(0, 0, -5);
+let sphere1_mat = new material([0.025, 0.04, 0.075], [0.1, 0.3, 0.5], [0.3, 0.3, 0.3], 16);
+let sphere1 = new sphere(new vec3(1, 0, 25), 0.3, sphere1_mat);
+
+let sphere2_mat = new material([0.075, 0, 0], [0.5, 0, 0], [0.3, 0.3, 0.3], 16);
+let sphere2 = new sphere(new vec3(0, 0, 25), 0.9, sphere2_mat);
+
+let sphere3_mat = new material ([0, 0.075, 0], [0, 0.5, 0], [0.3, 0.3, 0.3], 16);
+let sphere3 = new sphere(new vec3(-1, 1, 25), 0.15, sphere3_mat);
+
+let scene_objects = [sphere1, sphere2, sphere3];
 
 //Timing data
 let start_time = Date.now();
@@ -96,8 +130,21 @@ function getPixelIndex(x, y) {
 	return (x + y*WINDOW_WIDTH)*4;
 }
 
+function sdf_union(point, scene_objs) {
+	let f = MAX_DEPTH;
+	let mat = null;
+	for (let obj of scene_objs) {
+		let obj_dist = obj.sdf(point);
+		if (obj_dist < f) {
+			mat = obj.material;
+			f = obj_dist;
+		}
+	}
+	return new sdf_record(f, mat);
+}
+
 function sdf(point) {
-	return Math.min(vec3.sub(sphere_center, point).length - sphere_radius, vec3.sub(sphere2_center, point).length - sphere2_radius);
+	return sdf_union(point, scene_objects);
 }
 
 function marchRay(r) {
@@ -106,13 +153,13 @@ function marchRay(r) {
 
 	while (march_depth < MAX_DEPTH && steps < MAX_STEPS) {
 		let int_pt = r.stepBy(march_depth);
-		let f = sdf(int_pt);
+		let step_record = sdf(int_pt);
 		
-		march_depth += f;
+		march_depth += step_record.distance;
 		steps++;
 
-		if (f < EPSILON) {
-			return int_pt;
+		if (step_record.distance < EPSILON) {
+			return new intersection_record(int_pt, step_record.material);
 		}
 	}
 
@@ -122,8 +169,10 @@ function marchRay(r) {
 function update() {
 	elapsed_time = Date.now() - start_time;
 	let sphere_offset_angle = 2*Math.PI*(elapsed_time/10000);
-	sphere_center = new vec3(sphere_center_start.x + Math.cos(sphere_offset_angle), sphere_center_start.y + Math.sin(sphere_offset_angle), sphere_center_start.z + Math.sin(sphere_offset_angle*2)*5);
-	sphere2_center = new vec3(sphere2_center_start.x + Math.cos(sphere_offset_angle+Math.PI), sphere2_center_start.y + Math.sin(sphere_offset_angle+Math.PI), sphere2_center_start.z + Math.sin(sphere_offset_angle*2+Math.PI)*5);
+//	sphere1.origin = new vec3(sphere1.init_origin.x + Math.cos(sphere_offset_angle), sphere1.init_origin.y + Math.sin(sphere_offset_angle), sphere1.init_origin.z + Math.sin(sphere_offset_angle*2)*5);
+	sphere2.origin = new vec3(sphere2.init_origin.x, sphere2.init_origin.y + Math.sin(sphere_offset_angle/3)*0.2, sphere2.init_origin.z);
+	sphere1.origin = new vec3(sphere2.origin.x + Math.sin(sphere_offset_angle*1.2-Math.PI/2)*2, sphere2.origin.y + Math.cos(sphere_offset_angle)*2, sphere2.origin.z + Math.sin(sphere_offset_angle)*2);
+	sphere3.origin = new vec3(sphere1.origin.x + Math.sin(sphere_offset_angle*3+Math.PI/2)*0.5, sphere1.origin.y + Math.cos(sphere_offset_angle*3)*0.5, sphere1.origin.z + Math.sin(sphere_offset_angle*3)*0.5);
 
 	for (let y = 0; y < WINDOW_HEIGHT; y++) {
 		for (let x = 0; x < WINDOW_WIDTH; x++) {
@@ -134,12 +183,15 @@ function update() {
 			pixelData[i+3] = 255;
 
 			let view_ray = new ray(camera_center, vec3.sub(new vec3(x/WINDOW_WIDTH-0.5, y/WINDOW_HEIGHT-0.5, 0), camera_center));
-			let int_pt = marchRay(view_ray);
+			let int_record = marchRay(view_ray)
 
-			if (int_pt !== null) {
-				let grad_x = sdf(new vec3(int_pt.x + EPSILON, int_pt.y, int_pt.z)) - sdf(new vec3(int_pt.x - EPSILON, int_pt.y, int_pt.z));
-				let grad_y = sdf(new vec3(int_pt.x, int_pt.y + EPSILON, int_pt.z)) - sdf(new vec3(int_pt.x, int_pt.y - EPSILON, int_pt.z));
-				let grad_z = sdf(new vec3(int_pt.x, int_pt.y, int_pt.z + EPSILON)) - sdf(new vec3(int_pt.x, int_pt.y, int_pt.z - EPSILON));
+			if (int_record !== null) {
+				let int_pt = int_record.intersection_point;
+				let mat = int_record.material;
+
+				let grad_x = sdf(new vec3(int_pt.x + EPSILON, int_pt.y, int_pt.z)).distance - sdf(new vec3(int_pt.x - EPSILON, int_pt.y, int_pt.z)).distance;
+				let grad_y = sdf(new vec3(int_pt.x, int_pt.y + EPSILON, int_pt.z)).distance - sdf(new vec3(int_pt.x, int_pt.y - EPSILON, int_pt.z)).distance;
+				let grad_z = sdf(new vec3(int_pt.x, int_pt.y, int_pt.z + EPSILON)).distance - sdf(new vec3(int_pt.x, int_pt.y, int_pt.z - EPSILON)).distance;
 				let gradient = new vec3(grad_x, grad_y, grad_z);
 				let normal = gradient.normalize();
 				let normal_ray = new ray(int_pt, normal);
@@ -147,15 +199,16 @@ function update() {
 				let to_light = vec3.sub(light_center, int_pt);
 				let dist_to_light = to_light.length;
 				to_light = to_light.normalize();
-				let ray_to_light = new ray(normal_ray.stepBy(EPSILON), to_light);
-				let to_light_int = marchRay(ray_to_light);
+				let ray_to_light = new ray(normal_ray.stepBy(EPSILON*2), to_light);
+				let to_light_int_record = marchRay(ray_to_light);
 
-				if (to_light_int !== null) {
+				if (to_light_int_record !== null) {
+					let to_light_int = to_light_int_record.intersection_point;
 					let dist_to_light_int = vec3.sub(to_light_int, int_pt).length;
 					if (dist_to_light_int < dist_to_light) {
-						pixelData[i] = sphere_ambient[0]*light_color[0]*255;
-						pixelData[i+1] = sphere_ambient[1]*light_color[1]*255;
-						pixelData[i+2] = sphere_ambient[2]*light_color[2]*255;
+						pixelData[i] = mat.ambient[0]*light_color[0]*255;
+						pixelData[i+1] = mat.ambient[1]*light_color[1]*255;
+						pixelData[i+2] = mat.ambient[2]*light_color[2]*255;
 						continue;
 					}
 				}
@@ -163,19 +216,19 @@ function update() {
 				let view = view_ray.dir.neg().normalize();
 				let bisector = vec3.add(view, to_light).normalize();
 
-				let r = sphere_ambient[0]*light_color[0];
-				let g = sphere_ambient[1]*light_color[1];
-				let b = sphere_ambient[2]*light_color[2];
+				let r = mat.ambient[0]*light_color[0];
+				let g = mat.ambient[1]*light_color[1];
+				let b = mat.ambient[2]*light_color[2];
 
 				let diffuse_factor = Math.max(0, vec3.dot(normal, to_light));
-				r += sphere_diffuse[0]*light_color[0]*diffuse_factor;
-				g += sphere_diffuse[1]*light_color[1]*diffuse_factor;
-				b += sphere_diffuse[2]*light_color[2]*diffuse_factor;
+				r += mat.diffuse[0]*light_color[0]*diffuse_factor;
+				g += mat.diffuse[1]*light_color[1]*diffuse_factor;
+				b += mat.diffuse[2]*light_color[2]*diffuse_factor;
 
-				let spec_factor = Math.pow(Math.max(0, vec3.dot(normal, bisector)), sphere_specpow);
-				r += sphere_specular[0]*light_color[0]*spec_factor;
-				g += sphere_specular[1]*light_color[1]*spec_factor;
-				b += sphere_specular[2]*light_color[2]*spec_factor;
+				let spec_factor = Math.pow(Math.max(0, vec3.dot(normal, bisector)), mat.specpow);
+				r += mat.specular[0]*light_color[0]*spec_factor;
+				g += mat.specular[1]*light_color[1]*spec_factor;
+				b += mat.specular[2]*light_color[2]*spec_factor;
 
 				if (r > 1) r = 1;
 				if (g > 1) g = 1;
